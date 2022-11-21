261bc2ebb3b88257be64bf9d96e4cefe6f7f9614
fredboys <freddieboys@hotmail.co.uk>
2022-11-21 17:37:36 +0000

    Add task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,4 +3,13 @@
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
